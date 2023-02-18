ee9cb276ea5da15534d022a0549bbaa0eaebc8b6
MrRezoo <rezam578@gmail.com>
2023-02-18 17:10:14 +0330

    Chore: 💥 i18n in django

--- Django/shop/app/views.py
+++ Django/shop/app/views.py
@@ -1,5 +1,4 @@
-from django.db.models import Sum
-from django.http import JsonResponse
+from django.db.models import F, Sum
 from django.shortcuts import get_object_or_404
 
 from app.models import Order
@@ -7,6 +6,12 @@ from app.models import Order
 
 def checkout(request, order_pk):
     order = get_object_or_404(Order, pk=order_pk)
-    price = order.orderitem_set.aggregate(total_price=Sum('product__price'))
-    price['total_price'] = str(price['total_price'])
-    return JsonResponse(price)
+    order_items = order.orderitem_set.select_related('product')
+    # total_price = order_items.aggregate(
+    #     total_price=Sum(F('product__price') * F('quantity'))
+    # )
+    total_price = sum(
+       [item.product.price * item.quantity for item in order_items]
+    )
+    return {'total_price': f'{total_price["total_price"]:.2f}'}
+
