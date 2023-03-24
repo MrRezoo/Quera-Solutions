90e0f2f503df8eba552641dc4ca657a7ad229324
MrRezoo <rezam578@gmail.com>
2023-03-25 01:17:13 +0330

    💥Fix and push freeze_time

--- Django/migration/blog/models.py
+++ Django/migration/blog/models.py
@@ -1,23 +1,23 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils import timezone
 
-from config import settings
-
 
 class Article(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    STATUS_CHOICES = (
+        ('d', 'draft'),
+        ('p', 'published')
+    )
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
-    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey('Category', null=True, on_delete=models.SET_NULL)
     body = models.TextField()
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default='d')
     created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    published = models.DateTimeField(default=timezone.now)
 
 
 class Category(models.Model):
-    STATUS_CHOICES = (
-        ('d', 'draft'),
-        ('p', 'publish'),
-    )
     title = models.CharField(max_length=50)
-    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default='d')
-    published = models.DateTimeField(default=timezone.now)
-    updated = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=True)
