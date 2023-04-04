021223ce4661fe0f06a436715bcb88975fb697e6
MrRezoo <rezam578@gmail.com>
2023-04-04 10:11:40 +0330

    CommandLine 100% 🐳 fix collectCompany.py
    Signed-off-by: MrRezoo <rezam578@gmail.com>

--- Django/commandline/career/management/commands/collectCompany.py
+++ Django/commandline/career/management/commands/collectCompany.py
@@ -1,12 +1,14 @@
 import csv
 import os
+
 from django.conf import settings
 from django.core.management.base import BaseCommand
+
 from career.models import Company
 
 
 class Command(BaseCommand):
-    help = 'Collects contact information for all companies and saves it to a CSV file.'
+    help = 'Collects information of all companies and writes to a csv file.'
 
     def handle(self, *args, **options):
         companies = Company.objects.all()
@@ -15,18 +17,5 @@ class Command(BaseCommand):
 
         with open(filepath, mode='w') as csvfile:
             writer = csv.writer(csvfile)
-            writer.writerow(['name', 'email', 'phone'])
             for company in companies:
                 writer.writerow([company.name, company.email, company.phone])
-
-    # def handle(self, *args, **options):
-    #     companies = Company.objects.all()
-    #     filename = 'company.csv'
-    #     filepath = Path(settings.BASE_DIR) / filename
-    #
-    #     with open(filepath, mode='w') as csv_file:
-    #         fieldnames = ['name', 'email', 'phone']
-    #         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-    #         writer.writeheader()
-    #         for company in companies:
-    #             writer.writerow({'name': company.name, 'email': company.email, 'phone': company.phone})
