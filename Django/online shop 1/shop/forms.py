08e3c6a8ce52004f37db9a9a447d5b944bbdf12b
Reza Mobaraki <rezam578@gmail.com>
2024-08-04 11:54:00 +0330

    Django: new online shop (custom forms)

--- Django/online shop 1/shop/forms.py
+++ Django/online shop 1/shop/forms.py
@@ -1,33 +1,33 @@
 from django import forms
+from .models import Product, Color
 
 
 class CartForm(forms.Form):
     def __init__(self, *args, **kwargs):
         items = kwargs.pop('items', [])
-        super(CartForm, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
+
         for item in items:
-            self.fields['number_%s' % item.id] = forms.IntegerField(
+            self.fields[f'number_{item.id}'] = forms.IntegerField(
                 label=item.name,
                 initial=1,
                 required=False
             )
-            self.fields['color_%s' % item.id] = forms.ChoiceField(
+            self.fields[f'color_{item.id}'] = forms.ChoiceField(
                 label='color',
                 choices=[(color.id, color.name) for color in item.colors_available.all()],
                 required=False
             )
 
-    def is_valid(self):
-        valid = super(CartForm, self).is_valid()
-        if not valid:
-            return valid
-
-        for field_name, field in self.fields.items():
-            if field_name.startswith('number_') and not self.cleaned_data.get(field_name):
-                self.cleaned_data[field_name] = 1
-            elif field_name.startswith('color_') and not self.cleaned_data.get(field_name):
-                product_id = field_name.split('_')[1]
-                product = next(item for item in self.items if str(item.id) == product_id)
-                self.cleaned_data[field_name] = min(product.colors_available.all(), key=lambda color: color.name).id
-
-        return True
+    def clean(self):
+        cleaned_data = super().clean()
+        for field in self.fields:
+            if field.startswith('number_') and not cleaned_data.get(field):
+                cleaned_data[field] = 1
+            if field.startswith('color_') and not cleaned_data.get(field):
+                product_id = field.split('_')[1]
+                product = Product.objects.get(id=product_id)
+                colors = product.colors_available.all()
+                if colors.exists():
+                    cleaned_data[field] = colors.order_by('name').first().id
+        return cleaned_data
